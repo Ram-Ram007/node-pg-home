d68aa732c4e3e675b6020f586a173b09dbfb30ac
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-22 23:42:40 +0530

    modified

--- index.js
+++ index.js
@@ -46,43 +46,54 @@ app.patch("/update-user", async function (req, res) {
   });
 });
 app.get("/fav", async function (req, res) {
-    try {
-      let query = "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
+  try {
+    let query =
+      "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
 
-      //http://localhost:5001/fav?user_id=2
-      if (req.query.user_id) {
-        query += ` WHERE favourites.user_id = ${req.query.user_id}`;
+    //http://localhost:5001/fav?user_id=2
+    if (req.query.user_id) {
+      query += ` WHERE favourites.user_id = ${req.query.user_id}`;
     }
-  
-      // Adding search
-      //http://localhost:5001/fav?search=Cashews
-      if (req.query.search) {
-        if (req.query.category) {
-          query += ` AND`;
-        } else {
-          query += ` WHERE`;
-        }
-        query += ` items.item_name ILIKE '%${req.query.search}%'`;
+
+    // Adding search
+    //http://localhost:5001/fav?search=Cashews
+    if (req.query.search) {
+      if (req.query.category) {
+        query += ` AND`;
+      } else {
+        query += ` WHERE`;
       }
-  
-      // Adding sorting
-      if (req.query.sortBy) {
+      query += ` items.item_name ILIKE '%${req.query.search}%'`;
+    }
+
+    // Adding sorting
+    if (req.query.orderBy) {
         const sortOrder = req.query.sortOrder === "desc" ? "DESC" : "ASC";
-        query += ` ORDER BY ${req.query.sortBy} ${sortOrder}`;
+        query += ` ORDER BY ${req.query.orderBy} ${sortOrder}`;
+      }
+
+         // Adding sorting by price range
+         //http://localhost:5001/fav?priceRange=0-500
+    if (req.query.priceRange) {
+        const priceRanges = req.query.priceRange.split("-");
+        const minPrice = parseFloat(priceRanges[0]);
+        const maxPrice = parseFloat(priceRanges[1]);
+  
+        query += ` AND items.price BETWEEN ${minPrice} AND ${maxPrice}`;
       }
   
-      const pgRes = await pgClient.query(query);
-  
-      res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-      });
-    } catch (error) {
-      console.error("Error fetching items:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-  
+    const pgRes = await pgClient.query(query);
+
+    res.json({
+      rows: pgRes.rows,
+      count: pgRes.rowCount,
+    });
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.delete("/remove", async function (req, res) {
   const pgRes = await pgClient.query(
     "DELETE from users where userid=$1 RETURNING userid",
