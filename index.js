c26fb4aa63c8707ec33f4e9f00dadaf5b2684445
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-22 22:01:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const pgClient = require("./pg-config");
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.post("/save-user", async function (req, res) {
+  const queryText = "INSERT INTO users(name) VALUES($1) RETURNING userid,name";
+  const pgRes = await pgClient.query(queryText, [req.body.name]);
+
+  const postQueryText =
+    "INSERT INTO posts(postcontent,userid) VALUES($1,$2) RETURNING postid";
+  const postPgRes = await pgClient.query(postQueryText, [
+    req.body.postcontent,
+    pgRes.rows[0].userid,
+  ]);
+
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+    postInsert: postPgRes.rows,
+  });
+});
+
+app.patch("/update-user", async function (req, res) {
+  const queryText =
+    "UPDATE users set name=$1 where userid=$2 RETURNING userid,name";
+  const pgRes = await pgClient.query(queryText, [
+    req.body.name,
+    req.body.userid,
+  ]);
+
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
+});
+
+app.get("/fav", async function (req, res) {
+    try {
+      const pgRes = await pgClient.query(
+        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id",
+        
+      );
+  
+      res.json({
+        rows: pgRes.rows,
+        count: pgRes.rowCount,
+      });
+    } catch (error) {
+      console.error("Error fetching items:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
+app.delete("/remove", async function (req, res) {
+  const pgRes = await pgClient.query(
+    "DELETE from users where userid=$1 RETURNING userid",
+    [req.query.userid]
+  );
+
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
+});
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `Server running at http://${process.env.HOST}:${process.env.PORT}/`
+  );
+});
